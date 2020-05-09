13e05eace81d998a975367018faca5bf4c1493d3
Fernando Greco <greco-fernando@hotmail.com>
2020-05-08 22:18:39 -0300

    8/5

--- src/app/osasco/osasco.component.ts
+++ src/app/osasco/osasco.component.ts
@@ -14,12 +14,14 @@ export class OsascoComponent implements OnInit {
     var myChart = new Chart("myChart", {
       type: 'line',
       data: {
-          labels: ['16/3', '25/4', '26/4','27/4', '28/4','29/4','30/4','01/05','2/5','3/5','4/5','5/5'],
+          labels: ['16/3', '25/4', '26/4','27/4', '28/4','29/4','30/4','01/05','2/5','3/5','4/5','5/5','6/5'],
           datasets: [{
               label: 'Confirmados',
-              data: [497, 544, 549, 931, 929, 1023,1067,1093,1123,1352,1350,1403],
+              data: [497, 544, 549, 931, 929, 1023,1067,1093,1123,1352,1350,1403,1444,1561],
               backgroundColor: [
                   'rgba(255, 255, 255, 0)',
+                  'rgba(175, 33, 210, 1)',
+                  'rgba(175, 33, 210, 1)',
                   'rgba(175, 33, 210, 1)',
                   'rgba(175, 33, 210, 1)',
                   'rgba(175, 33, 210, 1)',
@@ -44,15 +46,19 @@ export class OsascoComponent implements OnInit {
                   'rgba(175, 33, 210, 1)',
                   'rgba(175, 33, 210, 1)',
                   'rgba(175, 33, 210, 1)',
+                  'rgba(175, 33, 210, 1)',
+                  'rgba(175, 33, 210, 1)',
                   'rgba(175, 33, 210, 1)'
               ],
               borderWidth: 1
           },
           {
             label: 'Recuperados',
-            data: [376, 387, 436, 457, 530, 618,628,635,648,743,780,802],
+            data: [376, 387, 436, 457, 530, 618,628,635,648,743,780,802,899,927],
             backgroundColor: [
                 'rgba(255, 255, 255, 0)',
+                'rgba(20, 210, 16, 1)',
+                'rgba(20, 210, 16, 1)',
                 'rgba(20, 210, 16, 1)',
                 'rgba(20, 210, 16, 1)',
                 'rgba(20, 210, 16, 1)',
@@ -78,6 +84,8 @@ export class OsascoComponent implements OnInit {
                 'rgba(20, 210, 16, 1)',
                 'rgba(20, 210, 16, 1)',
                 'rgba(20, 210, 16, 1)',
+                'rgba(20, 210, 16, 1)',
+                'rgba(20, 210, 16, 1)',
                 'rgba(20, 210, 16, 1)'
             ],
          
@@ -86,9 +94,11 @@ export class OsascoComponent implements OnInit {
 
         {
           label: 'Óbitos',
-          data: [51, 57, 68, 81, 89,91,104,108,111,118,134,140],
+          data: [51, 57, 68, 81, 89,91,104,108,111,118,134,140,145,149],
           backgroundColor: [
               'rgba(255, 255, 255, 0)',
+              'rgba(0, 0, 0, 1)',
+              'rgba(0, 0, 0, 1)',
               'rgba(0, 0, 0, 1)',
               'rgba(0, 0, 0, 1)',
               'rgba(0, 0, 0, 1)',
@@ -103,6 +113,8 @@ export class OsascoComponent implements OnInit {
 
           ],
           borderColor: [
+              'rgba(0, 0, 0, 1)',
+              'rgba(0, 0, 0, 1)',
               'rgba(0, 0, 0, 1)',
               'rgba(0, 0, 0, 1)',
               'rgba(0, 0, 0, 1)',
@@ -143,15 +155,20 @@ export class OsascoComponent implements OnInit {
   var myChart = new Chart("isolamentoSocial", {
     type: 'line',
     data: {
-        labels: ['24/4', '25/4', '26/4','27/4', '28/4','29/4','30/4','01/05','2/5','3/5','4/5'],
+        labels: ['24/4', '25/4', '26/4','27/4', '28/4','29/4','30/4','01/05','2/5','3/5','4/5','5/5','6/5','7/5'],
         datasets: [{
            
 
     
       label: 'Isolamento Social',
-      data: [50, 55, 61, 51, 50, 50, 49, 58,54,62,50,100],
+      data: [50, 55, 61, 51, 50, 50, 49, 58,54,62,50,50,50,50,100],
       backgroundColor: [
         'rgba(255, 255, 255, 0)',
+          'rgba(0, 0, 255, 1)',
+          'rgba(0, 0, 255, 1)',
+          'rgba(0, 0, 255, 1)',
+          'rgba(0, 0, 255, 1)',
+          'rgba(0, 0, 255, 1)',
           'rgba(0, 0, 255, 1)',
           'rgba(0, 0, 255, 1)',
           'rgba(0, 0, 255, 1)',
@@ -175,7 +192,12 @@ export class OsascoComponent implements OnInit {
         'rgba(0, 0, 255, 1)',
         'rgba(0, 0, 255, 1)',
         'rgba(0, 0, 255, 1)',
-        'rgba(0, 0, 255, 1)'
+        'rgba(0, 0, 255, 1)',
+        'rgba(0, 0, 255, 1)',
+        'rgba(0, 0, 255, 1)',
+          'rgba(0, 0, 255, 1)',
+          'rgba(0, 0, 255, 1)',
+          'rgba(0, 0, 255, 1)'
       ],
    
       borderWidth: 1
